94c4c0337f554348783134d9a6cd612b47edfba2
kdebbie <kehindeayeni41@gmail.com>
2021-03-22 14:27:15 +0100

    init

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,8 +14,8 @@ suite("Functional Tests", function () {
         .request(server)
         .get("/hello")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello Guest");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -23,52 +23,87 @@ suite("Functional Tests", function () {
     test("Test GET /hello with your name", function (done) {
       chai
         .request(server)
-        .get("/hello?name=xy_z")
+        .get("/hello?name=Kehinde")
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, "hello xy_z");
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Kehinde');
           done();
         });
     });
     // #3
-    test('send {surname: "Colombo"}', function (done) {
-      chai
-        .request(server)
-        .put("/travellers")
+test('send {surname: "Colombo"}', function(done) {
+   chai
+    .request(server)
+    .put('/travellers')
+    .send({ surname: 'Colombo' })
+    .end(function(err, res) {
+     assert.equal(res.status, 200, 'response status should be 200');
+      assert.equal(res.type, 'application/json', 'Response should be json');
+      assert.equal(
+        res.body.name,
+        'Cristoforo',
+        'res.body.name should be "Christoforo"'
+      );
+      assert.equal(
+        res.body.surname,
+        'Colombo',
+        'res.body.surname should be "Colombo"'
+      );
 
-        .end(function (err, res) {
-          assert.fail();
-
-          done();
-        });
+      done(); 
     });
+});
     // #4
-    test('send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+    test('send {surname: "da Verrazzano"}', function(done) {
+    chai
+    .request(server)
+    .put('/travellers')
+    .send({ surname: 'da Verrazzano' })
+    .end(function(err, res) {
+      assert.equal(res.status, 200, 'response status should be 200');
+      assert.equal(res.type, 'application/json', 'Response should be json');
+      assert.equal(res.body.name, 'Giovanni');
+      assert.equal(res.body.surname, 'da Verrazzano');
 
       done();
     });
-  });
 });
 
 const Browser = require("zombie");
+const browser = new Browser();
+const site = "https://boilerplate-mochachai.kehindealuko.repl.co"
 
-suite("Functional Tests with Zombie.js", function () {
+suite("Functional Tests with localhost", function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
-
-        done();
-      });
-    });
-    // #6
-    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      assert.fail();
-
+    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
+    browser.visit(site).then(() => {
+      browser.fill('surname', 'Colombo').then(() => {
+      browser.pressButton('submit', function() {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.element('span#dates', 1);
+          });
+        })});
       done();
-    });
+      
+    })
   });
-});
+
+    // #6
+    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+    browser.visit(site).then(() => {
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', function() {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.element('span#dates', 1);
+        });
+      })
+    });
+    done();
+  });
+})})});
